98262b272d6850039991d8e2a4e9442bd3446dd4
LtSnapping <son.tm99@gmail.com>
2020-02-29 19:03:37 -0500

    Updated routes/index

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,22 @@
-const express = require("express");
-const router = express.Router();
+const router = require("express").Router();
 
-/* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
-});
+const setupApi = client => {
+    const attachDb = api => require(api)(client)
+    
+    router.use('/books', attachDb('./books'));
+    /*router.use('/orders', attachDb('./orders'));
+    router.use('/inventory', attachDb('./inventory'));
+    router.use('/publishers', attachDb('./publishers'));
+    router.use('/sales-reports', attachDb('./sales-reports'));
+    router.use('/registration', attachDb('./registration'));
+    router.use('/profile', attachDb('./profile'));
+    router.use('/login', attachDb('./login'));
+    router.use('/reset-password', attachDb('./reset-password'));
+    router.use('/payment', attachDb('./payment'));
+    router.use('/modify-inventory', attachDb('./modify-inventory'));
+    router.use('/modify-publisher', attachDb('./modify-publisher'));*/
+    
+    return router;
+}
 
-module.exports = router;
+module.exports = setupApi;
