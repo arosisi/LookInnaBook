0687029506e977d217b53d2b1041e033385ce276
Tom Nguyen <tom.trinh.nguyen@gmail.com>
2020-03-17 22:06:36 -0400

    bug fix: backend error from login with wrong credentials

--- server/routes/login.js
+++ server/routes/login.js
@@ -18,38 +18,39 @@ module.exports = client => {
                     payload.send({ success: false, errMessage: "Failed to fetch from database" })
                 } else if (res.rows.length < 1) {
                     payload.send({ success: false, errMessage: "Couldn't find an user with that email and password" })
+                } else {
+                    const userQuery = 
+                    `SELECT 
+                        profile.u_id,
+                        profile.role,
+                        profile.first_name,
+                        profile.last_name,
+                        profile.address,
+                        profile.email,
+                        profile.password,
+                        credit_card_info.card_number,
+                        credit_card_info.expiry_date,
+                        credit_card_info.cvv,
+                        credit_card_info.holder_name,
+                        credit_card_info.billing_address
+                    FROM profile, credit_card, credit_card_info 
+                    WHERE profile.u_id = credit_card.u_id AND
+                          profile.card_number = credit_card.card_number AND
+                          credit_card.card_number = credit_card_info.card_number AND
+                          email = '${email}' AND 
+                          password = '${password}'
+                    LIMIT 1`
+                    client.query(userQuery, (err, res) => {
+                        if (err) {
+                            payload.send({ success: false, errMessage: "Failed to fetch from database" });
+                        } else {
+                            payload.send({ success: true, user: res.rows[0] })
+                        }
+                    })
                 }
             }
         )
         
-        const userQuery = 
-        `SELECT 
-            profile.u_id,
-            profile.role,
-            profile.first_name,
-            profile.last_name,
-            profile.address,
-            profile.email,
-            profile.password,
-            credit_card_info.card_number,
-            credit_card_info.expiry_date,
-            credit_card_info.cvv,
-            credit_card_info.holder_name,
-            credit_card_info.billing_address
-        FROM profile, credit_card, credit_card_info 
-        WHERE profile.u_id = credit_card.u_id AND
-              profile.card_number = credit_card.card_number AND
-              credit_card.card_number = credit_card_info.card_number AND
-              email = '${email}' AND 
-              password = '${password}'
-        LIMIT 1`
-        client.query(userQuery, (err, res) => {
-            if (err) {
-                payload.send({ success: false, errMessage: "Failed to fetch from database" });
-            } else {
-                payload.send({ success: true, user: res.rows[0] })
-            }
-        })
     })
     return router
 }
