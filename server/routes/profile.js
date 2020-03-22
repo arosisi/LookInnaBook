ff64c7c154ae7392dd474f91dc22feadbbefd55a
LtSnapping <son.tm99@gmail.com>
2020-03-21 21:13:52 -0400

    Removed console.log

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -69,7 +69,6 @@ module.exports = client => {
         }
         
         const updateCard = nextCall => {
-            console.log(creditCard)
             if (!creditCard) {
                 nextCall()
                 return
