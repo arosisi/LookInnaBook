cb5eb440ea50405f5ffc476bef663d1acd5b75ed
LtSnapping <son.tm99@gmail.com>
2020-03-21 23:28:39 -0400

    Small fix

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -161,7 +161,7 @@ module.exports = client => {
                         })
                     })
                 }
-            }
+            })
         }
         
         //The actual transaction
